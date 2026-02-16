44dc952ce8a7f68387c2e44b5cdca0a21ce4173c
Ijhar Ali <i4izharali9838@gmail.com>
2026-02-16 09:48:03 -0800

    Izhar Dev initial commit

---
+++ src/main/java/admin/panel/AdminPanelBackendApplication.java
@@ -0,0 +1,13 @@
+package admin.panel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminPanelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminPanelBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/admin/panel/AdminPanelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package admin.panel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminPanelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
